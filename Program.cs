0a2f54a4241fcea8753571a44afbdc6d083e537f
Trenton Isenor <ronesinotnert@gmail.com>
2021-11-15 14:33:31 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Properties Demo:");
+            Console.WriteLine();
+            Console.ReadKey(true);
+        }
+    }
+}
