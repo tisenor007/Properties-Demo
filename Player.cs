3b34ea266554272d064569e2850f4f16c7c751ab
Trenton Isenor <ronesinotnert@gmail.com>
2021-11-15 15:19:27 -0400

    added different types of properties

---
+++ Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Player
+    {
+        //public readonly string name;
+
+        private string _name = "Player";
+        public string Name
+        {
+            get { return _name; }
+        }
+        private int health;
+        private int _shield;
+        
+        //Actual GETS AND SETS
+        //could also add range checking.....
+        public int Shield
+        {
+            get { return _shield; }
+            set { _shield = value; }
+        }
+
+        //GET and SET methods
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        public void SetHealth(int value)
+        {
+            health = value;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,6 +12,31 @@ namespace Properties
         {
             Console.WriteLine("Properties Demo:");
             Console.WriteLine();
+            Player player = new Player();
+            //player.health = 100;
+            player.SetHealth(100);
+            
+            //Examples
+            player.SetHealth(1);
+
+            if (player.GetHealth() == 0)
+            {
+                //player dies.......
+            }
+
+            player.Shield = 99999;
+
+            if (player.Shield == 0)
+            {
+                //shield breaks
+            }
+
+            //CAN'T DO THIS
+            //player.Name = "falseName";
+
+            Console.WriteLine("Player Name: " + player.Name);
+            Console.WriteLine("Player Shield: " + player.Shield);
+            Console.WriteLine("Player Health: " + player.GetHealth());
             Console.ReadKey(true);
         }
     }
